8b512c88bf90357a5793c68953b3c968114f4b3e
TuxSH <1922548+TuxSH@users.noreply.github.com>
2020-05-05 00:29:18 +0100

    Add compile time options

--- source/main.c
+++ source/main.c
@@ -9,6 +9,13 @@
 
 #include "kernelhaxcode_3ds_bin.h"
 
+#ifndef DEFAULT_PAYLOAD_FILE_OFFSET
+#define DEFAULT_PAYLOAD_FILE_OFFSET 0
+#endif
+#ifndef DEFAULT_PAYLOAD_FILE_NAME
+#define DEFAULT_PAYLOAD_FILE_NAME   "SafeB9SInstaller.bin"
+#endif
+
 typedef struct ExploitChainLayout {
     u8 workBuf[0x10000];
     BlobLayout blobLayout;
@@ -17,11 +24,6 @@ typedef struct ExploitChainLayout {
 static Result doExploitChain(ExploitChainLayout *layout, Handle gspHandle, const char *payloadFileName, size_t payloadFileOffset)
 {
     Result res = 0;
-
-    Handle handle;
-    u32 baseSbufId;
-    u32 numSbufs;
-    u32 sbufs[16 * 2]; // max allowed by kernel
 
     memset(layout, 0, sizeof(ExploitChainLayout));
     memcpy(layout->blobLayout.code, kernelhaxcode_3ds_bin, kernelhaxcode_3ds_bin_size);
@@ -34,14 +36,23 @@ static Result doExploitChain(ExploitChainLayout *layout, Handle gspHandle, const
     if (kernelVersionMinor < 48) {
         // Below 9.3 -- memchunkhax
         TRY(memchunkhax(&layout->blobLayout, layout->workBuf, gspHandle));
-    } else {
+    } 
+
+#ifndef MEMCHUNKHAX_ONLY // reduces the size by around 3KB
+    else {
         // Above 9.3 -- pwn http (target TLS addresses are hard to guess below 4.x) and use LazyPixie
         // NOTE: this can be used without any required change from at least system version 4.2
+        Handle handle;
+        u32 baseSbufId;
+        u32 numSbufs;
+        u32 sbufs[16 * 2]; // max allowed by kernel
+
         baseSbufId = 4; // needs to be >= 4
         numSbufs = lazyPixiePrepareStaticBufferDescriptors(sbufs, baseSbufId);
         TRY(httpwn(&handle, baseSbufId, layout->workBuf, sbufs, numSbufs, gspHandle));
         TRY(lazyPixieTriggerArbwrite(&layout->blobLayout, handle, baseSbufId));
     }
+#endif
 
     // https://developer.arm.com/docs/ddi0360/e/memory-management-unit/hardware-page-table-translation
     // "MPCore hardware page table walks do not cause a read from the level one Unified/Data Cache"
@@ -67,8 +78,8 @@ Result otherappMain(u32 paramBlkAddr)
         arm9PayloadFileOffset = *(size_t *)(paramBlkAddr + 0xEFC);
         arm9PayloadFileName = (const char *)(paramBlkAddr + 0xF00); // max 255 characters
     } else {
-        arm9PayloadFileOffset = 0;
-        arm9PayloadFileName = "SafeB9SInstaller.bin";
+        arm9PayloadFileOffset = DEFAULT_PAYLOAD_FILE_OFFSET;
+        arm9PayloadFileName = DEFAULT_PAYLOAD_FILE_NAME;
     }
 
     ExploitChainLayout *layout = (ExploitChainLayout *)((paramBlkAddr + 0x1000) & ~0xFFF);
